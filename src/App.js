64469261d14e73c15ad61bcbaba550769310fc43
Nurkanat <nurkanat.doszhan@gmail.com>
2021-03-03 19:39:43 +0600

    using react-sortable with bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+import 'bulma/css/bulma.css';
 
-function App() {
+const SortableItem = SortableElement(({value}) => <li className="box">{value}</li>);
+
+const SortableList = SortableContainer(({items}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
+    </ul>
   );
+});
+
+class App extends Component {
+  state = {
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+  };
+
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState(({items}) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">
+          Hello World
+        </h1>
+        <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+      </div>
+    );
+  }
 }
 
 export default App;
